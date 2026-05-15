e9fd4f06c9973aed782390528d8790c53e96ae90
shiamrahman2 <mohammodshiambhuiyan@gmail.com>
2026-05-15 12:57:18 +0600

    SHA-256 convertion

--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"encoding/base64"
+	"crypto/sha256"
+	//"encoding/base64"
 	"fmt"
 )
 
@@ -9,43 +10,11 @@ import (
 // 	"ecomerce/cmd"
 // )
 
-/*
-handleCORS and handlePreflightRequest don't required because i handle CORS and Preflight With global MUX
-
-	func handleCORS(w http.ResponseWriter) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "POST,GET,PATCH,DELETE,OPTIONS,PUT")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-		w.Header().Set("Content-Type", "application/json")
-	}
-
-	func handlePreflightRequest(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-	}
-*/
 func main() {
 	//cmd.Serve()
-	var s string
-	s = "Hello World"
-
-	byteArr := []byte(s)
-	fmt.Println("string-", s)
-	fmt.Println("byte Array-", byteArr)
-	enc := base64.URLEncoding
-	enc = enc.WithPadding(base64.NoPadding)
-
-	b64Str := enc.EncodeToString(byteArr)
-
-	fmt.Println("Base 64-", b64Str)
-
-	convertByte,err:=enc.DecodeString(b64Str)
-	if err!=nil{
-		fmt.Println(err)
-		return
-	}
-	fmt.Println("Converted Byte-",convertByte)
+	
+	data:=[]byte("Hello World")
+	EncryptedData:=sha256.Sum256(data)
+	fmt.Println(EncryptedData)
 
 }
